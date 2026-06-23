92522d046d7d44d1a9585b58cc993e442f9d93fb
yaohenrimichel27-afk <yaohenrimichel27@gmail.com>
2026-06-23 16:36:23 +0000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Admin from './Admin.jsx'
+
+// Simple routing without extra dependencies:
+// visit yoursite.vercel.app/admin to access the dashboard
+const isAdmin = window.location.pathname.startsWith('/admin')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <Admin /> : <App />}
   </React.StrictMode>
 )
